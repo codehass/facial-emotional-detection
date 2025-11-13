b35189d0ba93cdd2b368dcd6c52b4c864b128547
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-11-13 09:18:52 +0100

    create predictions end point

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,59 @@
-from fastapi import FastAPI, Depends, HTTPException
+import sys
+import os
+import uuid, shutil, aiofiles
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from fastapi import FastAPI, Depends, HTTPException, UploadFile, File
 from sqlalchemy.orm import Session
 from database import Base, engine, get_db
-from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.prediction_model import Prediction
+from deeplearning.scripts.detect_and_predict import emotion_detection
+from sqlalchemy import func
+
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 
-# Create a new patient
+# Create main route endpoint
 @app.get("/")
-async def get_home(db: AsyncSession = Depends(get_db)):
-    return {"message": "Hello from ecome project"}
+async def get_home():
+    return {"message": "Hello emotion facial detection API"}
+
+
+# create prediction endpoint
+@app.post("/predictions")
+async def create_prediction(
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db),
+):
+    images_type = ["image/jpeg", "image/png", "image/jpg"]
+
+    if file.content_type not in images_type:
+        raise HTTPException(status_code=400, detail="Only image files are allowed.")
+
+    file_ext = file.filename.split(".")[-1]
+    temp_filename = f"{uuid.uuid4()}.{file_ext}"
+
+    async with aiofiles.open(temp_filename, "wb") as out_file:
+        content = await file.read()
+        await out_file.write(content)
+
+    confidence, predicted_emotion = emotion_detection(temp_filename)
+
+    if os.path.exists(temp_filename):
+        os.remove(temp_filename)
+
+    new_prediction = Prediction(emotion=predicted_emotion, confidence=str(confidence))
+    db.add(new_prediction)
+    db.commit()
+    db.refresh(new_prediction)
+
+    confidence = float(confidence)
+
+    return {
+        "emotion": predicted_emotion,
+        "confidence": confidence,
+    }
