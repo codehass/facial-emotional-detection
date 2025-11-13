ac8571515286c1a9d09a106435d3f5033df48a54
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-11-13 21:04:16 +0100

    update main

--- backend/app/models/prediction_model.py
+++ backend/app/models/prediction_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy import Column, Integer, String, DateTime, Float
 from database import Base
 from pydantic import BaseModel
 from datetime import datetime, timezone
@@ -8,15 +8,8 @@ class Prediction(Base):
     __tablename__ = "predictions"
 
     id = Column(Integer, primary_key=True, index=True)
-    emotion = Column(String)
-    confidence = Column(String)
-    create_at = Column(
+    emotion = Column(String, nullable=False)
+    confidence = Column(Float, nullable=False)
+    created_at = Column(
         DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
     )
-
-
-# class PredictionResponse(BaseModel):
-#     int: id
-#     emotion: str
-#     confidence: str
-#     # create_at: DateTime
