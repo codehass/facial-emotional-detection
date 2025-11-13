185fa441db8b361ac26e3f01d0b4b707e9f8d3e8
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-11-13 21:04:05 +0100

    refactor main

--- backend/main.py
+++ backend/main.py
@@ -4,63 +4,17 @@ import uuid, aiofiles
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from fastapi import FastAPI, Depends, HTTPException, UploadFile, File
-from sqlalchemy.orm import Session
-from database import Base, engine, get_db
-from app.models.prediction_model import Prediction
-from deeplearning.scripts.detect_and_predict import emotion_detection
-from sqlalchemy import func
-
-
-app = FastAPI()
+from fastapi import FastAPI
+from database import Base, engine
+from app.routes import prediction_routes
 
 Base.metadata.create_all(bind=engine)
 
+app = FastAPI(title="Emotion Detection API", version="1.0.0")
 
-# Create main route endpoint
-@app.get("/")
-async def get_home():
-    return {"message": "Hello emotion facial detection API"}
+app.include_router(prediction_routes.router)
 
 
-# create prediction endpoint
-@app.post("/predict_emotion")
-async def create_prediction(
-    file: UploadFile = File(...),
-    db: Session = Depends(get_db),
-):
-    images_type = ["image/jpeg", "image/png", "image/jpg"]
-
-    if file.content_type not in images_type:
-        raise HTTPException(status_code=400, detail="Only image files are allowed.")
-
-    file_ext = file.filename.split(".")[-1]
-    temp_filename = f"{uuid.uuid4()}.{file_ext}"
-
-    async with aiofiles.open(temp_filename, "wb") as out_file:
-        content = await file.read()
-        await out_file.write(content)
-
-    confidence, predicted_emotion = emotion_detection(temp_filename)
-
-    if os.path.exists(temp_filename):
-        os.remove(temp_filename)
-
-    new_prediction = Prediction(emotion=predicted_emotion, confidence=str(confidence))
-    db.add(new_prediction)
-    db.commit()
-    db.refresh(new_prediction)
-
-    confidence = float(confidence)
-
-    return {
-        "emotion": predicted_emotion,
-        "confidence": confidence,
-    }
-
-
-@app.get("/history")
-async def get_history(db: Session = Depends(get_db)):
-    history = db.query(Prediction).all()
-
-    return {"history": history}
+@app.get("/", tags=["Root"])
+async def root():
+    return {"message": "Welcome to the Emotion Facial Detection API!"}
